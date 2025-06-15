e000b04d8311a688cf62909269a940f600d51d5d
ariel ben pazi <rbenpazi@gmail.com>
2025-06-15 11:34:27 +0300

    Add project files.

---
+++ sansorProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sansorProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
